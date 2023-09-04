60dafb4d82abecca065acbc0eeb7c77ef7235699
Julian Gamboa <juliandg34@gmail.com>
2023-09-04 18:04:58 -0500

    Adding GreetResponse class

--- src/main/java/com/jdgr/Main.java
+++ src/main/java/com/jdgr/Main.java
@@ -1,15 +1,13 @@
 package com.jdgr;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// @SpringBootApplication
-@ComponentScan
-@EnableAutoConfiguration
+import java.util.Objects;
+
+@SpringBootApplication
 @RestController
 public class Main {
     public static void main(String[] args) {
@@ -17,7 +15,42 @@ public class Main {
     }
 
     @GetMapping("/")
-    public String greet() {
-        return "Hello";
+    public GreetResponse greet() {
+        return new GreetResponse("Hello");
+    }
+
+    // record GreetResponse(String greet) { }
+
+    class GreetResponse {
+        private final String greet;
+
+
+        GreetResponse(String greet) {
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public String toString() {
+            return "GreetResponse{" +
+                    "greet='" + greet + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GreetResponse that = (GreetResponse) o;
+            return Objects.equals(greet, that.greet);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(greet);
+        }
     }
 }
